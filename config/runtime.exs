b796f811996dff4cd65382879db623d21e521327
TheZabotage <jeppe.zabo@it-minds.dk>
2026-04-15 12:01:29 +0200

    mailer = gone

--- config/runtime.exs
+++ config/runtime.exs
@@ -116,7 +116,5 @@ if config_env() == :prod do
   #     config :swoosh, :api_client, Swoosh.ApiClient.Req
   #
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
-  config :it_whist, ItWhist.Mailer,
-    adapter: Swoosh.Adapters.Resend,
-    api_key: System.get_env("RESEND_API_KEY") || "not-configured"
+  config :it_whist, ItWhist.Mailer, adapter: Swoosh.Adapters.Test
 end
